ce8f3ea554a61fc8e4c3b5c370973abe6c6f9e69
Lazurusaki <163603284+Lazurusaki@users.noreply.github.com>
2024-03-17 13:17:36 +0300

    02_GenerateEnemies script edited

--- Assets/Scripts/02_GenerateEnemies/EnemySpawner.cs
+++ Assets/Scripts/02_GenerateEnemies/EnemySpawner.cs
@@ -1,11 +1,12 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] Transform[] _spawnPoints;
-    [SerializeField] GameObject _enemyPrefab;
-    [SerializeField] float _spawnInterval;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private float _spawnInterval;
 
     private float _rotationMin = 0f;
     private float _rotationMax = 360f;   
@@ -25,7 +26,7 @@ public class EnemySpawner : MonoBehaviour
         while (true)
         {
             int spawnPointIndex = Random.Range(0, _spawnPoints.Length);
-            GameObject enemy = Instantiate(_enemyPrefab, _spawnPoints[spawnPointIndex].position, Quaternion.identity);
+            GameObject enemy = Instantiate(_enemyPrefab.gameObject, _spawnPoints[spawnPointIndex].position, Quaternion.identity);
             Quaternion rotation = Quaternion.Euler(0f, Random.Range(_rotationMin, _rotationMax), 0.0f);
             enemy.transform.rotation = rotation; 
             yield return wait;
